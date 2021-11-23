1e2cdc7ab04c9628105b2f286f7ea18810e1ca91
Rubem Mota <rubemmota89@gmail.com>
2021-11-23 21:43:38 +0000

    added newer python versions into lint

--- fabdb_sdk/helpers/api_client.py
+++ fabdb_sdk/helpers/api_client.py
@@ -11,12 +11,16 @@ class ApiClient:
     def __init__(self):
         self.__base_url = __baseurl__
         self.__client = requests.Session()
-        self.__client.request = functools.partial(self.__client.request, timeout=3)
+        self.__client.request = functools.partial(
+            self.__client.request,
+            timeout=3
+        )
 
     def request(self, endpoint):
         try:
             response = self.__client.get(
-                self.__prepare_url(endpoint), headers={"Accept": "application/json"}
+                self.__prepare_url(endpoint),
+                headers={"Accept": "application/json"}
             )
             if response.status_code == 404:
                 raise NotFoundException("not found")
